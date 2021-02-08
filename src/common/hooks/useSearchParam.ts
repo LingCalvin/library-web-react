3787d93315c77d67dff057c0746f12388ca6eb95
Calvin Ling <calvinling8@gmail.com>
2021-02-08 15:40:31 -0500

    Add documentation

--- src/common/hooks/useSearchParam.ts
+++ src/common/hooks/useSearchParam.ts
@@ -1,5 +1,11 @@
 import { useLocation } from 'react-router-dom';
 
+/**
+ * This hook gives access to the specified search parameter.
+ *
+ * @param param - The search parameter
+ * @returns The value of the search parameter if it exists. `null` otherwise.
+ */
 export default function useSearchParam(param: string) {
   const { search } = useLocation();
   return new URLSearchParams(search).get(param);
