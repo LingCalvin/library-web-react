30779e9be936e500aea00df6ed6fbf7d952423be
Calvin Ling <calvinling8@gmail.com>
2021-02-08 14:47:02 -0500

    Restructure exports

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Login from './login';
-import ForgotPassword from './forgot-password';
-import ResetPassword from './reset-password';
-import Home from './home';
-import NotFound from './not-found';
+import Login from './login/login';
+import ForgotPassword from './forgot-password/forgot-password';
+import ResetPassword from './reset-password/reset-password';
+import Home from './home/home';
+import NotFound from './not-found/not-found';
 
 export default function App() {
   return (
